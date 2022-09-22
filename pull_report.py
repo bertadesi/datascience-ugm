497c8ba1aea067bdaf356ac15d4686bbea064c76
bertadesi <88239561+bertadesi@users.noreply.github.com>
2022-09-22 18:19:31 +0700

    Demo Show Report

---
+++ pull_report.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 22 08:59:51 2022
+
+@author: AS00340968
+"""
+
+import InsertDB
+import matplotlib.pyplot as plt
+
+hasil=InsertDB.pullReport()
+
+Detail_Category=[]
+Sum_Amount=[]
+for row in hasil:
+    
+    print(row[0])
+    Detail_Category.append(row[0])
+    Sum_Amount.append((row[1]))
+    # print(row['category'])
+     
+print("Label of Category = ", Detail_Category)
+print("Sum of Transaction = ", Sum_Amount)
+ 
+ 
+#Visulizing Data using Matplotlib
+plt.bar(Detail_Category, Sum_Amount)
+plt.xlabel("Label of Categories")
+plt.ylabel("Sum of Amount")
+plt.title("Total Expense Spent")
+plt.show()
