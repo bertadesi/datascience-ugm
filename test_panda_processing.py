42c3fcec4fe999a57eead3e2dc1024d9b3a3adca
bertadesi <88239561+bertadesi@users.noreply.github.com>
2022-09-30 22:28:41 +0700

    Add Evaluation Metric
    put lower case in predict model

--- test_panda_processing.py
+++ test_panda_processing.py
@@ -6,7 +6,7 @@ Created on Mon Sep 19 19:28:27 2022
 """
 
 import pandas as pd
-emailcategory = pd.read_csv('email_set.csv')
+emailcategory = pd.read_csv('email_set_model.csv')
 
 from io import StringIO
 import csv
@@ -30,22 +30,40 @@ emailcategory['subject']= emailcategory['subject'].replace('\n',' ', regex=True)
 
 emailcategory.to_csv(r'email_set_labelled.txt', index=False, sep=' ', header=False, quoting=csv.QUOTE_NONE, quotechar="", escapechar=" ")
 
-model = fasttext.train_supervised('email_set_labelled.txt',epoch=25)
+model = fasttext.train_supervised('email_set_labelled.txt',epoch=25,lr=1.0,wordNgrams=1)
 
-test_hasil=model.predict('indihome',k=1,threshold=0.0,on_unicode_error='strict')
-#print(test_hasil)
+
+test_hasil=model.predict('mandiri utama finance',k=1,threshold=0.0,on_unicode_error='strict')
+
+# print(test_hasil)
+
 
 #print(test_hasil[0][0])
 
 def predict_Text(text):
     
-    hasil_predict=model.predict(text,k=1,threshold=0.0,on_unicode_error='strict')
+    
+    hasil_predict=model.predict(text.lower(),k=1,threshold=0.0,on_unicode_error='strict')
     
     
     return hasil_predict[0][0]
 
 
+def print_results(model, input_path, k):
+    num_records, precision_at_k, recall_at_k = model.test(input_path, k)
+    f1_at_k = 2 * (precision_at_k * recall_at_k) / (precision_at_k + recall_at_k)
 
-# hasil_test = model.test('email_valid.txt',k=1,threshold=0.0)
+    print("records\t{}".format(num_records))
+    print("Precision@{}\t{:.3f}".format(k, precision_at_k))
+    print("Recall@{}\t{:.3f}".format(k, recall_at_k))
+    print("F1@{}\t{:.3f}".format(k, f1_at_k))
+    print()
+    
 
-# print(hasil_test)
+for k in range(1, 3):
+    print('train metrics:')
+    print_results(model, 'email_train.txt', k)
+
+    print('test metrics:')
+    print_results(model, 'email_valid.txt', k)
+
